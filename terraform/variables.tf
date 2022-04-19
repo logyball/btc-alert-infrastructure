563492d9d8f7f9cbcbe3bdbd55bcfbed083debcf
Logan Ballard <loganballard@gmail.com>
2022-04-18 23:14:18 -0400

    adding tf fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "env_name" {}
 
 variable "vm_size" {
-    default = "t2.micro"
+  default = "t2.micro"
 }
 
 locals {
-    vm_name = "btc-tracker-${var.env_name}"
+  vm_name = "btc-tracker-${var.env_name}"
 }
